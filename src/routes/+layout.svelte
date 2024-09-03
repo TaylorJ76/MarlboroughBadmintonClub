fd803306646b4b4235c9226aef3b5da0f9da4ebc
Nick Darlow <dangerous.darlow@gmail.com>
2024-09-03 09:30:33 +0100

    Add header and main sections to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-	import '../app.postcss';
+    import '../app.postcss';
 
-	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+    // Floating UI for Popups
+    import {arrow, autoUpdate, computePosition, flip, offset, shift} from '@floating-ui/dom';
+    import {storePopup} from '@skeletonlabs/skeleton';
+
+    storePopup.set({computePosition, autoUpdate, flip, shift, offset, arrow});
 </script>
 
-<slot />
+<div class="ml-4 mr-4">
+    <header>
+        Marlborough Badminton
+    </header>
+    <main>
+        <slot/>
+    </main>
+</div>
