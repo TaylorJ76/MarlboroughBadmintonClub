98ab5be92670d7b67c932b596345163407368062
Nick Darlow <dangerous.darlow@gmail.com>
2024-09-09 18:54:50 +0100

    Use LightSwitch to select light / dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
-	import '../app.postcss';
+    import '../app.postcss';
 
-	// Floating UI for Popups
-	import { arrow, autoUpdate, computePosition, flip, offset, shift } from '@floating-ui/dom';
-	import { autoModeWatcher, storePopup } from '@skeletonlabs/skeleton';
+    // Floating UI for Popups
+    import {arrow, autoUpdate, computePosition, flip, offset, shift} from '@floating-ui/dom';
+    import {LightSwitch, storePopup} from '@skeletonlabs/skeleton';
 
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+    storePopup.set({computePosition, autoUpdate, flip, shift, offset, arrow});
 </script>
 
-<svelte:head>
-	{@html '<script>(' + autoModeWatcher.toString() + ')();</script>'}
-</svelte:head>
 <div class="ml-4 mr-4">
-	<header>Marlborough Badminton</header>
-	<main>
-		<slot />
-	</main>
+    <header>
+        <div class="flex items-center justify-between pt-1 pb-1">
+            <h1>Marlborough Badminton</h1>
+            <LightSwitch/>
+        </div>
+    </header>
+    <main>
+        <slot/>
+    </main>
 </div>
