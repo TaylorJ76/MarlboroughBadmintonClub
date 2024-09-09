5dbd9deb4b5bcf15cf932114f4bb58cd72db495b
Nick Darlow <dangerous.darlow@gmail.com>
2024-09-09 19:02:41 +0100

    Update formatting configuration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-    import '../app.postcss';
+  import '../app.postcss';
 
-    // Floating UI for Popups
-    import {arrow, autoUpdate, computePosition, flip, offset, shift} from '@floating-ui/dom';
-    import {LightSwitch, storePopup} from '@skeletonlabs/skeleton';
+  // Floating UI for Popups
+  import { arrow, autoUpdate, computePosition, flip, offset, shift } from '@floating-ui/dom';
+  import { LightSwitch, storePopup } from '@skeletonlabs/skeleton';
 
-    storePopup.set({computePosition, autoUpdate, flip, shift, offset, arrow});
+  storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
 <div class="ml-4 mr-4">
-    <header>
-        <div class="flex items-center justify-between pt-1 pb-1">
-            <h1>Marlborough Badminton</h1>
-            <LightSwitch/>
-        </div>
-    </header>
-    <main>
-        <slot/>
-    </main>
+  <header>
+    <div class="flex items-center justify-between pt-1 pb-1">
+      <h1>Marlborough Badminton</h1>
+      <LightSwitch />
+    </div>
+  </header>
+  <main>
+    <slot />
+  </main>
 </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,21 +6,18 @@ import { skeleton } from '@skeletonlabs/tw-plugin';
 import { marlboroughTheme } from './src/marlborough-theme';
 
 export default {
-	darkMode: 'media',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
-	],
-	theme: {
-		extend: {}
-	},
-	plugins: [
-		forms,
-		typography,
-		skeleton({
-			themes: {
-				custom: [marlboroughTheme]
-			}
-		})
-	]
+  darkMode: 'media',
+  content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    forms,
+    typography,
+    skeleton({
+      themes: {
+        custom: [marlboroughTheme]
+      }
+    })
+  ]
 } satisfies Config;
