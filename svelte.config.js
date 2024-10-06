fd990f6c9da53cce16e4133587e6c9e33f039962
Nick Darlow <dangerous.darlow@gmail.com>
2024-10-06 15:53:20 +0100

    Add base path /MarlboroughBadmintonClub if not development environment

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/marlboroughbadmintonclub.github.io' : ''
+      base: process.env.NODE_ENV === 'development' ? '' : '/MarlboroughBadmintonClub'
     }
   }
 };
