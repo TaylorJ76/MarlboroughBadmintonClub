9eaca2b9e2bc57701f74c78e50f18248ea6d6280
Nick Darlow <dangerous.darlow@gmail.com>
2024-10-06 15:57:30 +0100

    Removed conditional base path

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === 'development' ? '' : '/MarlboroughBadmintonClub'
+      base: '/MarlboroughBadmintonClub'
     }
   }
 };
