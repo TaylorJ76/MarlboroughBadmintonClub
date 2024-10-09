5594f679da6ac598d3551e7df220be1451aa0239
James Taylor <146064280+TaylorJ76@users.noreply.github.com>
2024-10-09 08:11:16 +0100

    Update Sidebar.svelte
    Updates sidebar with accessibility fixes

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,30 +1,51 @@
 <script lang="ts">
   import { fly } from 'svelte/transition';
-  import { Menu } from 'lucide-svelte';
+  import { Menu, CircleX } from 'lucide-svelte';
 
   let isOpen = false;
+  const openLabel = 'Open menu';
+  const closeLabel = 'Close menu';
+  let buttonLabel = openLabel;
 
   const toggleOpen = () => {
     isOpen = !isOpen;
+    buttonLabel = isOpen ? closeLabel : openLabel
   };
 </script>
 
-{#if isOpen}
-  <div class="fixed inset-0 bg-gray-50 bg-opacity-50 dark:bg-gray-900 dark:bg-opacity-50 z-40" on:click={toggleOpen} aria-hidden="true" />
-
-  <div
-    class="fixed top-0 left-0 w-64 bg-gray-100 dark:bg-gray-800 h-full shadow-lg z-50 p-6 transform"
-    in:fly={{ x: -300, duration: 300 }}
-    out:fly={{ x: -300, duration: 300 }}
-    on:click={toggleOpen}
-    aria-hidden="true"
-  >
-    <div class="pt-16">
-      <slot></slot>
-    </div>
-  </div>
-{/if}
-
-<button class="absolute left-2 top-2 p-3 z-50" on:click={toggleOpen}>
-  <Menu class="w-10 h-10" />
+<button 
+  class="md:hidden absolute left-2 top-2 p-3 z-30" 
+  on:click={toggleOpen} 
+  aria-label={buttonLabel} 
+  aria-controls="mobile-nav"
+>
+  {#if isOpen}
+    <CircleX class="w-10 h-10" />
+  {:else}
+    <Menu class="w-10 h-10" />
+  {/if}
 </button>
+
+<nav class="md:hidden">
+  <div 
+    hidden={!isOpen} 
+    class="fixed inset-0 bg-gray-50 bg-opacity-50 dark:bg-gray-900 dark:bg-opacity-50 z-10" 
+    on:click={toggleOpen} 
+    aria-hidden="true" 
+  />
+
+  {#if isOpen}
+    <div
+      id="mobile-nav"
+      class="fixed top-0 left-0 w-64 bg-gray-100 dark:bg-gray-800 h-full shadow-lg z-20 p-6 transform"
+      in:fly={{ x: -300, duration: 300 }}
+      out:fly={{ x: -300, duration: 300 }}
+      aria-labelledby="nav-label"
+      role="dialog"
+    >
+      <div class="pt-16">
+        <slot></slot>
+      </div>
+    </div>
+  {/if}
+</nav>
