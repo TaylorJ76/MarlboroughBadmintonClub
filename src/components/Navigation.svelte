25c87392a4da2f6a079ac0aa78ba20f7c95aa833
Nick Darlow <dangerous.darlow@gmail.com>
2024-10-09 08:42:03 +0100

    Remove override of a element on click
    It does not seem necessary to use goto for navigation

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import type { MenuItem } from '../types';
-  import { goto } from '$app/navigation';
   import { base } from '$app/paths';
   export let menuItems: MenuItem[] = [];
 </script>
@@ -9,7 +8,7 @@
   <ul>
     {#each menuItems as item}
       <li>
-        <a class="flex items-center p-2" href={base + item.route} on:click={(e) => { e.preventDefault(); goto(base + item.route); }}>
+        <a class="flex items-center p-2" href={base + item.route}>
           <span>{item.name}</span>
         </a>
       </li>
